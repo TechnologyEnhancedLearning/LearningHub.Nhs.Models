47080bf2b709058401c9ef33c2c20893dbba9ea8
binon <Binon.Yesudhas@hee.nhs.uk>
2023-12-28 23:00:42 +0000

    Commint log folder

---
+++ LearningHub.Nhs.Models/Log/LogViewModel.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="LogViewModel.cs" company="HEE.nhs.uk">
+// Copyright (c) HEE.nhs.uk.
+// </copyright>
+
+namespace LearningHub.Nhs.Models.Log
+{
+    /// <summary>
+    /// The log view model.
+    /// </summary>
+    public class LogViewModel : LogBasicViewModel
+    {
+        /// <summary>
+        /// Gets or sets the logger.
+        /// </summary>
+        public string Logger { get; set; }
+
+        /// <summary>
+        /// Gets or sets the callsite.
+        /// </summary>
+        public string Callsite { get; set; }
+
+        /// <summary>
+        /// Gets or sets the exception.
+        /// </summary>
+        public string Exception { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user id.
+        /// </summary>
+        public int UserId { get; set; }
+    }
+}
