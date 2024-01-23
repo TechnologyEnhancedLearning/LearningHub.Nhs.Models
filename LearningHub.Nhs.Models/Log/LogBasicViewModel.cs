442a3b4fb80b4564f0020ae674373f3c05a01bb7
binon <Binon.Yesudhas@hee.nhs.uk>
2024-01-23 13:34:31 +0000

    Add missing log folder

---
+++ LearningHub.Nhs.Models/Log/LogBasicViewModel.cs
@@ -0,0 +1,46 @@
+﻿// <copyright file="LogBasicViewModel.cs" company="HEE.nhs.uk">
+// Copyright (c) HEE.nhs.uk.
+// </copyright>
+
+namespace LearningHub.Nhs.Models.Log
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// The log basic view model.
+    /// </summary>
+    public class LogBasicViewModel
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the application.
+        /// </summary>
+        public string Application { get; set; }
+
+        /// <summary>
+        /// Gets or sets the logged.
+        /// </summary>
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy HH:mm:ss}", ApplyFormatInEditMode = true)]
+        public DateTime Logged { get; set; }
+
+        /// <summary>
+        /// Gets or sets the level.
+        /// </summary>
+        public string Level { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user name.
+        /// </summary>
+        public string UserName { get; set; }
+    }
+}
